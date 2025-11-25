17081a6a20f3b5ec483af4b87e1272fdc8a715e5
Shardul1719 <shardulj26@gmail.com>
2025-11-25 11:03:37 +0530

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,20 @@
+import TrailContainer from "@/components/TrailContainer";
+
+export default function Home() {
+  return (
+    <>
+      <section className="hero">
+        <div className="hero-img">
+          <img src="/hero.jpg" alt="" />
+        </div>
+        <p>[ The Future Moves in Frames ]</p>
+        <p>Experiment 457 by Saewine</p>
+
+        <TrailContainer />
+      </section>
+
+
+    </>
+  );
+    
+}
